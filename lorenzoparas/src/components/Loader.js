27ebca80bbdbc3bd28a4b890fb0afafb7a681b8f
Lorenzo Paras <ivesparas@gmail.com>
2022-08-08 22:48:30 +1000

    removed loading circle

--- lorenzoparas/src/components/Loader.js
+++ lorenzoparas/src/components/Loader.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import React from 'react';
 import 'react-circular-progressbar/dist/styles.css';
 import Typist from 'react-typist';
 import 'react-typist/dist/Typist.css';
+import { INTRO_AVG_TYPING_DELAY, INTRO_WELCOME_MESSAGE } from '../consts/IntroValues';
 
 const welcomeWrapperStyle = {
     backgroundColor: 'white',
@@ -19,58 +19,14 @@ const welcomeStyle = {
     fontSize: '64px'
 }
 
-const progressStyle = buildStyles({
-    pathTransitionDuration: 0.1,
-    pathColor: `grey`,
-    textColor: 'black',
-    trailColor: '#d6d6d6'
-});
-
-const spinnerStyle = {
-    height: '33vh',
-    width: '33vh'
-}
-
-const Loader = (loading) => {
-    const [percentage, setPercentage] = React.useState(0);
-    const [showMessage, setShowMessage] = React.useState(false);
-    let count = 0;
-
-    useState(() => {
-        let inc = setInterval(() => {
-            if (count >= 100) {
-                setPercentage(100);
-                clearInterval(inc);
-                setShowMessage(true);
-            } else {
-                count++;
-                setPercentage(perc => perc + 1);
-            }
-        }, (2000/100));
-
-    }, []);
-
+const Loader = () => {
     return (
         <div style={welcomeWrapperStyle}>
-            {
-                showMessage ?
-                <div style={welcomeStyle}>
-                    <Typist>
-                        welcome to my site!
-                    </Typist>
-                </div>
-                :
-                loading ?
-                <div style={spinnerStyle}>
-                    <CircularProgressbar
-                        value={percentage * 1.2}
-                        text={`${percentage}%`}
-                        styles={progressStyle}
-                    />
-                </div>
-                :
-                ""
-            }
+            <div style={welcomeStyle}>
+                <Typist avgTypingDelay={INTRO_AVG_TYPING_DELAY}>
+                    {INTRO_WELCOME_MESSAGE}
+                </Typist>
+            </div>
         </div>
     );
 }
