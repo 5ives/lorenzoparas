a3f88050fd6f4024fa8ac16319192a2f36d287e7
Lorenzo Paras <ivesparas@gmail.com>
2022-08-15 21:59:52 +1000

    [LOR-17] init s3 bucket endpoints

---
+++ lorenzoparas/src/services/AWSS3Service.ts
@@ -0,0 +1,56 @@
+import { ListObjectsCommand } from "@aws-sdk/client-s3";
+import s3Client from "../config/aws/s3";
+
+const s3BucketName = process.env.REACT_APP_AWS_S3_BUCKET_NAME;
+
+export const getAlbums = async () => {
+    try {
+        const data = await s3Client.send(
+            new ListObjectsCommand({
+                Delimiter: "/",
+                Bucket: s3BucketName
+            })
+        );
+
+        if (!data || !data.CommonPrefixes) return [];
+
+        const albumNames = data.CommonPrefixes.map(album => {
+            const albumPrefix = album.Prefix;
+            return albumPrefix ? decodeURIComponent((albumPrefix).replace('/', '')) : ''
+        });
+
+        const nonEmptyalbumNames = albumNames.filter(albumName => albumName.length > 0);
+
+        return nonEmptyalbumNames;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export const getImages = async (albumName: string, limit?: number) => {
+    albumName = albumName.trim();
+
+    try {
+        const data = await s3Client.send(
+            new ListObjectsCommand({
+                Prefix: albumName,
+                Bucket: s3BucketName,
+                MaxKeys: limit
+            })
+        );
+
+        if (!data || !data.Contents) return [];
+
+        const href = "https://s3." + process.env.REACT_APP_AWS_REGION + ".amazonaws.com/";
+        const bucketUrl = href + s3BucketName + "/";
+        const photos = data.Contents.map(photo => {
+            const photoKey = photo.Key || '';
+            const photoUrl = bucketUrl + encodeURIComponent(photoKey);
+            return photoUrl;
+        });
+
+        return photos
+    } catch (error) {
+        console.error(error);
+    }
+};
